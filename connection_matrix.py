aaec65c5f06bd298bd3cfd859da42af6a28cff48
such-a-git <adam.perrett@manchester.ac.uk>
2019-10-17 18:13:59 +0100

    VM filter can now be used to generate a connection matrix of desired downsampling and spatial resolution, only thing left is overlap and second layer combining of separate filters rotations and shit

--- connection_matrix.py
+++ connection_matrix.py
@@ -20,79 +20,125 @@ Components:
 x_res = 304
 y_res = 240
 
-def banana(x, y, a=1, b=100):
-    return 1. / (np.power(a - x, 2) + (b*np.power(y - np.power(x, 2), 2)))
-
-def circle(x, y, radius=0.5, origin_x=0.25, origin_y=0.25, stdev=0.1):
-    if y > origin_y*2:
-        circle_value = 0 #y - origin_y
+def zero_2pi_tan(x, y, theta=0):
+    angle = np.arctan2(y, x)
+    if angle < 0:
+        angle += np.pi * 2
+    if angle > 6 * np.pi / 4:
+        angle -= 6 * np.pi / 4
     else:
-        circle_value = np.power(x - np.sqrt(origin_x), 2) + np.power(y - np.sqrt(origin_y), 2)
-    distance_from_circle = abs(radius - circle_value)
-    # some gauss stuffx`
-    return 1./(distance_from_circle+0.1), norm.ppf(distance_from_circle+0.001)
-    # return distance_from_circle, norm.ppf(distance_from_circle+0.001)
+        angle += np.pi / 2
+    return angle #+ theta
 
-def VM(x, y, r=10, p=0.08, theta=0):
-    x *= 50
-    y *= 50
+# Von Mises filter function, takes values between -1 and 1 and outputs the corresponding value
+def VM(x, y, r=10, p=0.08, theta=0, threshold=0.75, filter_split=7):
+    theta *= np.pi/4
+    # rescaling of x and y for correct filter placement
+    x = (x*15) + (0.8*r) * np.cos(theta)
+    y = (y*15) - (0.8*r) * np.sin(theta)
+    # slit the value for the filter
+    angle = zero_2pi_tan(x, y, theta)
+    if (angle >= np.pi - theta and angle <= (2*np.pi) - theta):
+        split = -1
+    else:
+        split = int((angle + theta) / (np.pi / float(filter_split))) % filter_split
     numerator = np.exp(p * r * np.cos(np.arctan2(-y, x) - theta))
-    # if np.square(x) + np.square(y) < r:
-    #     denominator = np.sqrt(abs(np.square(x) + np.square(y) - r))
-    # elif np.square(x) + np.square(y) == r:
-    #     denominator = 0.01
-    # else:
     denominator = np.i0(np.sqrt(np.square(x) + np.square(y)) - r)
     if denominator == 0:
         denominator = 0.01
-    return numerator / denominator
+    VM_output = numerator / denominator
+    # threshold to get rid of small tail
+    if VM_output < threshold:
+        VM_output = 0
+    else:
+        VM_output = 1#-= threshold
+    return VM_output, split
+
+# Creates a matrix of values for a particular resolution and rotation of the VM filter
+def generate_matrix(resolution=46, rotation=0, plot=False):
+    rotation_matrix = []
+    banana_matrix = []
+    xs = []
+    ys = []
+    for i in range(-resolution, resolution):
+        x = []
+        y = []
+        banana_row = []
+        rotation_row = []
+        for j in range(-resolution, resolution):
+            x.append(float(i) / float(resolution))
+            y.append(float(j) / float(resolution))
+            banan, split = VM(float(i)/float(resolution), float(j)/float(resolution), theta=rotation)
+            banana_row.append(round(banan, 2))
+            rotation_row.append(split)
+        xs.append(x)
+        ys.append(y)
+        banana_matrix.append(banana_row)
+        rotation_matrix.append(rotation_row)
+
+    if plot:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_wireframe(np.array(xs), np.array(ys), np.array(banana_matrix))
+        ax.plot_wireframe(np.array(xs), np.array(ys), np.array(rotation_matrix))
+        plt.show()
+    return banana_matrix
+
+# down_sample = generate_matrix(20, rotation=0, plot=True)
+# down_sample = generate_matrix(20, rotation=1, plot=True)
+# down_sample = generate_matrix(20, rotation=2, plot=True)
+# down_sample = generate_matrix(20, rotation=3, plot=True)
+
+def convert_pixel_to_id(x, y):
+    return (y*304) + x
+
+def generate_visual_field(filter_width, filter_height, filter_split=4, rotation=0, plot=False):
+    # create inputs of -1 to 1 for entire visual field, assuming no overlap for now
+    filter_x_res = x_res / filter_width
+    filter_y_res = y_res / filter_height
+    connection_list = [[] for i in range(filter_width * filter_height * filter_split)]
+    visual_matrix = []
+    xs = []
+    ys = []
+    for i in range(x_res):
+        visual_row = []
+        x = []
+        y = []
+        for j in range(y_res):
+            x_value = (float(i % filter_x_res) / float(filter_x_res / 2)) - 1.
+            y_value = (float(j % filter_y_res) / float(filter_y_res / 2)) - 1.
+            pixel_value, split = VM(x_value, y_value, theta=rotation, filter_split=filter_split)
+            if pixel_value > 0 and split >= 0:
+                neuron_index = (int(y_res / filter_y_res) * filter_width) + int(x_res / filter_x_res) + filter_split
+                connection_list.append([convert_pixel_to_id(i, j), neuron_index])
+            x.append(i)
+            y.append(j)
+            visual_row.append(pixel_value)
+        visual_matrix.append(visual_row)
+        xs.append(x)
+        ys.append(y)
+
+    if plot:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_wireframe(np.array(xs), np.array(ys), np.array(visual_matrix))
+        plt.show()
+    return visual_matrix
+
+# generate_visual_field(3, 2, rotation=1, plot=True)
 
 
-resolution = 100
-banana_matrix = []
-value_matrix = []
-xs = []
-ys = []
-for i in range(-resolution, resolution):
-    x = []
-    y = []
-    banana_row = []
-    value_row = []
-    for j in range(-resolution, resolution):
-        x.append(float(i) / float(resolution))
-        y.append(float(j) / float(resolution))
-        # value, banan = circle(float(i)/float(resolution), float(j)/float(resolution))
-        banan = VM(float(i)/float(resolution), float(j)/float(resolution))
-        banana_row.append(round(banan, 2))
-        # value_row.append(round(value, 2))
-    xs.append(x)
-    ys.append(y)
-    banana_matrix.append(banana_row)
-    # value_matrix.append(value_row)
 
-
-from mpl_toolkits.mplot3d import axes3d
-import matplotlib.pyplot as plt
-#
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-#
-# # Grab some test data.
-X, Y, Z = axes3d.get_test_data(0.05)
-#
-# # Plot a basic wireframe.
-# ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
-#
-# plt.show()
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.plot_wireframe(np.array(xs), np.array(ys), np.array(banana_matrix))
-# ax.plot_wireframe(np.array(xs), np.array(ys), np.array(value_matrix))
-
-plt.show()
+# print zero_2pi_tan(1, 1)
+# print zero_2pi_tan(0, 1)
+# print zero_2pi_tan(-1, 1)
+# print zero_2pi_tan(-1, 0)
+# print zero_2pi_tan(-1, -1)
+# print zero_2pi_tan(-0.01, -1)
+# print zero_2pi_tan(0, -1)
+# print zero_2pi_tan(0.01, -1)
+# print zero_2pi_tan(1, -1)
+# print zero_2pi_tan(1, 0)
 
 print "done"
 
