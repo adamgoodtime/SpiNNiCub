a8699d00bd9303ee81f8b9aaa76bed60c104786e
such-a-git <adam.perrett@manchester.ac.uk>
2019-10-19 00:36:31 +0100

    creates connection list for first layer of filter neurons with tunable overlap

--- connection_matrix.py
+++ connection_matrix.py
@@ -92,11 +92,11 @@ def generate_matrix(resolution=46, rotation=0, plot=False):
 def convert_pixel_to_id(x, y):
     return (y*304) + x
 
-def generate_visual_field(filter_width, filter_height, filter_split=4, rotation=0, plot=False):
+def generate_visual_field(no_filter_x, no_filter_y, filter_split=4, rotation=0, plot=False):
     # create inputs of -1 to 1 for entire visual field, assuming no overlap for now
-    filter_x_res = x_res / filter_width
-    filter_y_res = y_res / filter_height
-    connection_list = [[] for i in range(filter_width * filter_height * filter_split)]
+    filter_x_res = x_res / no_filter_x
+    filter_y_res = y_res / no_filter_y
+    connection_list = []
     visual_matrix = []
     xs = []
     ys = []
@@ -109,8 +109,9 @@ def generate_visual_field(filter_width, filter_height, filter_split=4, rotation=
             y_value = (float(j % filter_y_res) / float(filter_y_res / 2)) - 1.
             pixel_value, split = VM(x_value, y_value, theta=rotation, filter_split=filter_split)
             if pixel_value > 0 and split >= 0:
-                neuron_index = (int(y_res / filter_y_res) * filter_width) + int(x_res / filter_x_res) + filter_split
-                connection_list.append([convert_pixel_to_id(i, j), neuron_index])
+                neuron_index = int(y_res / filter_y_res) + (int(x_res / filter_x_res) * no_filter_y) + filter_split
+                filter_identifier = [int(y_res / filter_y_res), int(x_res / filter_x_res), filter_split, rotation]
+                connection_list.append([convert_pixel_to_id(i, j), neuron_index, filter_identifier])
             x.append(i)
             y.append(j)
             visual_row.append(pixel_value)
@@ -125,22 +126,105 @@ def generate_visual_field(filter_width, filter_height, filter_split=4, rotation=
         plt.show()
     return visual_matrix
 
+def create_filter_boundaries(filter_width, filter_height, overlap=0.):
+    list_of_corners = []
+    i = filter_width
+    x = 0
+    while i < x_res:
+        j = filter_height
+        y = 0
+        while j < y_res:
+            list_of_corners.append([i, j, x, y])
+            j += filter_height - (overlap * filter_height)
+            y += 1
+        i += filter_width - (overlap * filter_width)
+        x += 1
+    return list_of_corners
+
+def visual_field_with_overlap(filter_width, filter_height, overlap=0., filter_split=4, rotation=0,
+                              base_weight=1, percentage_fire_threshold=0.5, plot=False):
+    # create inputs of -1 to 1 for entire visual field, assuming no overlap for now
+    filter_x_res = filter_width
+    filter_y_res = filter_height
+    overlap_x = filter_width * overlap
+    overlap_y = filter_height * overlap
+    max_filters_x = int(x_res / (filter_width / (1-overlap)))
+    max_filters_y = int(y_res / (filter_height / (1-overlap)))
+    centre_offset_x = filter_width / 2
+    centre_offset_y = filter_height / 2
+
+    corners_list = create_filter_boundaries(filter_width, filter_height, overlap)
+
+    filter_split_matrix = []
+    filter_matrix = []
+    xs = []
+    ys = []
+    for i in range(filter_width):
+        split_row = []
+        filter_row = []
+        x = []
+        y = []
+        for j in range(filter_height):
+            x_value = ((float(i) / float(filter_width)) * 2) - 1
+            y_value = ((float(j) / float(filter_height)) * 2) - 1
+            pixel_value, split = VM(x_value, y_value, theta=rotation, filter_split=filter_split)
+            x.append(x_value)
+            y.append(y_value)
+            filter_row.append(pixel_value)
+            split_row.append(split)
+        filter_split_matrix.append(split_row)
+        filter_matrix.append(filter_row)
+        xs.append(x)
+        ys.append(y)
+
+    visual_matrix = []
+    split_matrix = []
+    xs = []
+    ys = []
+    connection_list = []
+    neuron_id_count = [0 for i in range(max_filters_x * max_filters_y * filter_split)]
+    for corner in corners_list:
+        for filter_x in range(len(filter_matrix)):
+            x = []
+            y = []
+            visual_row = []
+            split_row = []
+            for filter_y in range(len(filter_matrix[0])):
+                x_offset = corner[0] - filter_width
+                y_offset = corner[1] - filter_height
+                x.append(x_offset + filter_x)
+                y.append(y_offset + filter_y)
+                visual_row.append(filter_matrix[filter_x][filter_y])
+                split_row.append(filter_split_matrix[filter_x][filter_y])
+                if filter_matrix[filter_x][filter_y] and filter_split_matrix[filter_x][filter_y] >= 0:
+                    pixel_value = convert_pixel_to_id(x_offset + filter_x, y_offset + filter_y)
+                    split_value = filter_split_matrix[filter_x][filter_y]
+                    neuron_id = (corner[2] * filter_split) + (max_filters_x * corner[3] * filter_split) + split_value
+                    print neuron_id
+                    neuron_id_count[neuron_id] += 1
+                    connection_list.append([pixel_value, neuron_id, base_weight, 1])
+            xs.append(x)
+            ys.append(y)
+            visual_matrix.append(visual_row)
+            split_matrix.append(split_row)
+
+    for connection in range(len(connection_list)):
+        connection_list[connection][2] /= neuron_id_count[connection_list[connection][1]] * percentage_fire_threshold
+
+    if plot:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_wireframe(np.array(xs), np.array(ys), np.array(visual_matrix))
+        plt.show()
+    return connection_list
+
 # generate_visual_field(3, 2, rotation=1, plot=True)
-
-
-
-# print zero_2pi_tan(1, 1)
-# print zero_2pi_tan(0, 1)
-# print zero_2pi_tan(-1, 1)
-# print zero_2pi_tan(-1, 0)
-# print zero_2pi_tan(-1, -1)
-# print zero_2pi_tan(-0.01, -1)
-# print zero_2pi_tan(0, -1)
-# print zero_2pi_tan(0.01, -1)
-# print zero_2pi_tan(1, -1)
-# print zero_2pi_tan(1, 0)
+first_layer_connections_r0 = visual_field_with_overlap(100, 100, 0., plot=False)
 
 print "done"
+
+
+
 
 
 
